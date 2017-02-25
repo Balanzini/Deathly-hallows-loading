76a54d0676f18b52b8b53b28e01b0ed13aa44f99
joseluisb <jlbalanza@droiders.com>
2017-02-25 14:14:23 +0100

    first commit

---
+++ app/src/main/java/com/balanza/android/loadinghp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.balanza.android.loadinghp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+
+  override fun onResume() {
+    super.onResume()
+//    my_loading.start()
+
+  }
+}
